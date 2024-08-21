43d32db27bc3f29e55aede45d5124708418e2fa4
StefanPatman <stefanpatman91@gmail.com>
2024-08-21 18:54:34 +0300

    Show log content in final report

--- src/itaxotools/abcd_validator/types.py
+++ src/itaxotools/abcd_validator/types.py
@@ -15,16 +15,32 @@ class LogType(Enum):
 class LogEntry:
     type: LogType
     text: str
+    content: dict[str, str]
 
     def __str__(self) -> str:
         prefix = {
             LogType.Warning: "\u2757",
             LogType.Error: "\u274C",
         }[self.type]
-        return f"{prefix} {self.text}"
+        return f"{prefix} {self.file_content_to_text()}{self.text} {self.message_content_to_text()} {repr(self.content)}"
 
     def to_text(self) -> str:
-        return f"{self.type.name}: {self.text}"
+        return f"{self.type.name}: {self.file_content_to_text()}{self.text} {self.message_content_to_text()} {repr(self.content)}"
+
+    def file_content_to_text(self) -> str:
+        file = self.content.get("file", "")
+        if not file:
+            return ""
+        if file == "result":
+            return "In scheme validation: "
+        return f"In {file.lower()} table: "
+
+    def message_content_to_text(self) -> str:
+        message = self.content.get("message", "")
+        if not message:
+            return ""
+        message = message.replace("\n", "")
+        return f"<{message}>"
 
 
 class ListLogger(Outputter):
@@ -33,5 +49,5 @@ class ListLogger(Outputter):
         self.type = type
 
     def handle(self, description, content):
-        entry = LogEntry(self.type, str(description))
+        entry = LogEntry(self.type, str(description), content)
         self.reference.append(entry)
